43dfd370ed35801392d3e0495b9589cc70399ac0
thiagoferreiraw <th.thiago96@gmail.com>
2017-10-02 23:03:19 -0300

    Deploy circle ci

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Deploying to heroku..."
+#git push heroku master
+heroku config:set DATABASE_NAME=$DATABASE_NAME
+heroku config:set DATABASE_USER=$DATABASE_USER
+heroku config:set DATABASE_PASSWORD=$DATABASE_PASSWORD
+heroku config:set DATABASE_HOST=$DATABASE_HOST
+heroku config:set DATABASE_PORT=$DATABASE_PORT
+heroku config:set SOCIAL_AUTH_FACEBOOK_KEY=$SOCIAL_AUTH_FACEBOOK_KEY
+heroku config:set SOCIAL_AUTH_FACEBOOK_SECRET=$SOCIAL_AUTH_FACEBOOK_SECRET
+heroku run python manage.py migrate
+heroku ps:scale web=1
