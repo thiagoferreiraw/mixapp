eb94fb92fa40824dd967457519724cc49e0b27b2
thiagobferreira-luizalabs <thiago.ferreira@luizalabs.com>
2017-10-04 13:33:09 -0300

    Added email and full name to sign up form

---
+++ app/core/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm as UserCreationFormDjango, User, UsernameField
+from django.conf import settings
+
+
+class UserCreationForm(UserCreationFormDjango):
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationFormDjango, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update({'autofocus': ''})
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email",)
+        field_classes = {'username': UsernameField}
+
+
--- app/core/views.py
+++ app/core/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm, UserCreationForm
+from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash, login, authenticate
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from app.core.forms import UserCreationForm
 
 from social_django.models import UserSocialAuth
 
