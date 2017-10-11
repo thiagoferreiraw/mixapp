a6f53b32be144e24a888f0a13a7d34faf88da4bc
thiagoferreiraw <th.thiago96@gmail.com>
2017-10-11 19:19:29 -0300

    tests

--- app/test_settings.py
+++ app/test_settings.py
@@ -5,8 +5,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME', 'circle_test'),
         'USER': os.environ.get('DATABASE_USER', 'ubuntu'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'root'),
-        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
         'CONN_MAX_AGE': 600,
     }
