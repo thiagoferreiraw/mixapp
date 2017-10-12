e6f5925c0bb1b26f02421f2a8d50a8c585d6614b
thiagoferreiraw <th.thiago96@gmail.com>
2017-10-12 12:10:53 -0300

    tests

--- app/test_settings.py
+++ app/test_settings.py
@@ -2,6 +2,12 @@ from app.settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME_TEST', 'postgres'),
+            'USER': os.environ.get('DATABASE_USER_TEST', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD_TEST', 'root'),
+        'HOST': os.environ.get('DATABASE_HOST_TEST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT_TEST', '5432'),
+        'CONN_MAX_AGE': 600,
     }
 }
