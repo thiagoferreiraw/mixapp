af60ff1d1d42946860b61f18d09ecad5b54cef96
thiagoferreiraw <th.thiago96@gmail.com>
2017-10-12 12:03:19 -0300

    tests

--- app/test_settings.py
+++ app/test_settings.py
@@ -3,8 +3,8 @@ from app.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
-        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'NAME': os.environ.get('DATABASE_NAME', 'DSADASDASDA'),
+        'USER': os.environ.get('DATABASE_USER', 'postDASDASDSADASDASDADgres'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
