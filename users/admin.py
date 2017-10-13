2ee2a16f74f5a280ef3b6d4eb0ca2f47c66c29ab
Thiago <th.thiago96@gmail.com>
2017-10-12 23:52:30 -0300

    user invitation

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from users.models import Category
+from users.models import Category, SignupInvitation
 
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
+class SignupInvitationAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(SignupInvitation, SignupInvitationAdmin)
 
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 
 class Category(models.Model):
@@ -9,9 +10,17 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class UserCategory(models.Model):
     user_id = models.ForeignKey(User)
     category_id = models.ForeignKey(Category)
 
     class Meta:
         unique_together = (("user_id", "category_id"),)
+
+
+class SignupInvitation(models.Model):
+    email_invited = models.EmailField()
+    user_has_signed_up = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True, blank=True, editable=True)
+    hash = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
