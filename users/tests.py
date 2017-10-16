04d497f7ce2484afda1b217d8e83e2c45049bc73
thiagobferreira-luizalabs <thiago.ferreira@luizalabs.com>
2017-10-16 14:36:30 -0200

    user tests

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, RequestFactory
-from users import views
-from unittest import mock
+from users.models import SignupInvitation
+
 
 class UserTests(TestCase):
 
@@ -16,11 +16,31 @@ class UserTests(TestCase):
         response = self.client.get('/home/', follow=True)
         self.assertRedirects(response, '/login/?next=/home/')
 
-    def test_signup_with_invalid_invitation(self):
+    def test_get_access_signup_pagewith_invalid_invitation(self):
         response = self.client.get('/signup/invalid_hash/')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['hash'], 'invalid_hash')
 
+    def test_get_signup_page_with_valid_invitation(self):
+        signup_invitation = SignupInvitation(email_invited="invited@test.com")
+        signup_invitation.save()
 
+        signup_url = '/signup/{}/'.format(signup_invitation.hash).replace("-", "")
 
+        response = self.client.get(signup_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['invitation'], signup_invitation)
+
+    def test_post_signup_page_with_valid_invitation(self):
+        signup_invitation = SignupInvitation(email_invited="invited@test.com")
+        signup_invitation.save()
+
+        signup_url = '/signup/{}/'.format(signup_invitation.hash).replace("-", "")
+
+        response = self.client.post(signup_url, {'email': 'invited@test.com',
+                                    })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['invitation'], signup_invitation)
