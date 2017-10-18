1aa80a9f2798faa84436523fc3ca2b3c9eb1f8d3
Thiago <th.thiago96@gmail.com>
2017-10-17 23:37:32 -0200

    Reestructure templates

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Deploying to heroku..."
-heroku git:remote -a aqueous-scrubland-68580
-#git push heroku master
 heroku config:set DATABASE_NAME=$DATABASE_NAME
 heroku config:set DATABASE_USER=$DATABASE_USER
 heroku config:set DATABASE_PASSWORD=$DATABASE_PASSWORD
@@ -9,5 +6,8 @@ heroku config:set DATABASE_HOST=$DATABASE_HOST
 heroku config:set DATABASE_PORT=$DATABASE_PORT
 heroku config:set SOCIAL_AUTH_FACEBOOK_KEY=$SOCIAL_AUTH_FACEBOOK_KEY
 heroku config:set SOCIAL_AUTH_FACEBOOK_SECRET=$SOCIAL_AUTH_FACEBOOK_SECRET
-heroku run python manage.py migrate
-heroku ps:scale web=1
+heroku config:set EMAIL_HOST=$EMAIL_HOST
+heroku config:set EMAIL_HOST_PASSWORD=$EMAIL_HOST_PASSWORD
+heroku config:set EMAIL_HOST_USER=$EMAIL_HOST_USER
+heroku config:set EMAIL_PORT=$EMAIL_PORT
+heroku config:set DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
