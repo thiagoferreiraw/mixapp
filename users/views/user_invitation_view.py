75b4833b71580feb58114f452f39f67f5362b25c
fnscoder <fnscoder@gmail.com>
2017-10-24 22:20:50 -0200

    test send invitation

--- users/views/user_invitation_view.py
+++ users/views/user_invitation_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from users.forms import UserInvitationForm
 from django.views.generic import View
 from users.models import SignupInvitation
@@ -10,10 +11,12 @@ from django.core.mail import send_mail
 class UserInvitationView(View):
     template_name = "registration/send_invitation.html"
 
+
     def get(self, request):
         form = UserInvitationForm()
         invitations = SignupInvitation.objects.filter(invited_by=request.user)
         return render(request, self.template_name, {'invitations': invitations, 'form': form})
+
 
     def post(self, request):
         form = UserInvitationForm(request.POST)
