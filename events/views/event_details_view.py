7c80205b3c3915cee972b19285c9b10095dc226c
fnscoder <fnscoder@gmail.com>
2017-11-08 21:53:03 -0200

    testing event details view

--- events/views/event_details_view.py
+++ events/views/event_details_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.views.generic import View
 from events.forms import Event
 
@@ -7,5 +8,13 @@ class EventDetailsView(View):
     template_name = "events/event_details.html"
 
     def get(self, request, event_id):
-        event_details = Event.objects.filter(id=event_id).first()
+        event_details = self.get_event_or_404(event_id)
+        
         return render(request, self.template_name, {'event_details': event_details})
+
+    @staticmethod
+    def get_event_or_404(event_id):
+        event = Event.objects.filter(pk=event_id).first()
+        if not event:
+            raise Http404()
+        return event
