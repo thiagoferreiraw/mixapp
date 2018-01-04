03d504620fef036b13a82b9b08496915a38b71be
Raian de Andrades <raian.andrades@gmail.com>
2018-01-04 18:28:43 -0200

    find + feedback pages

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index2/$', views.index2, name='indext2'),
     url(r'^find/$', views.find, name='find'),
+    url(r'^mixdetails/$', views.mixdetails, name='mixdetails'),
     url(r'^feedback/$', views.feedback, name='feedback'),
+    url(r'^feedbackdetails/$', views.feedbackdetails, name='feedbackdetails'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -9,5 +9,11 @@ def index2(request):
 def find(request):
       return render(request, "find.html", {})
 
+def mixdetails(request):
+      return render(request, "mix-details.html", {})
+
 def feedback(request):
       return render(request, "feedback.html", {})
+
+def feedbackdetails(request):
+      return render(request, "feedback-details.html", {})
