bea78aa0b62e096700aa9e9166ebe94bae72286b
Thiago <th.thiago96@gmail.com>
2018-01-28 13:52:44 -0200

    django machina setup

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
+from machina.app import board
 
 urlpatterns = [
-    url(r'', include('pages.urls')),
-    url(r'', include('users.urls')),
-    url(r'', include('events.urls')),
-    url(r'^admin/', admin.site.urls)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r'', include('pages.urls')),
+                  url(r'', include('users.urls')),
+                  url(r'', include('events.urls')),
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^forum/', include(board.urls)),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
